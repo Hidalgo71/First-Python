ba028b2bc15d23888916025fbb0e1b15686e9ddb
Hidalgo71 <y.ozdemir71@gmail.com>
2019-09-29 22:56:54 +0300

    updated.

--- source.py
+++ source.py
@@ -10,3 +10,9 @@ print(word, sent, paragraph)
 i = 3 + 6
 print("i=", i)
 
+int1 = input("1st number:")
+int2 = input("2nd number:")
+suM = int(int1) + int(int2)
+print(suM)
+print("2nd add:", int(int1) + int(int2))
+
