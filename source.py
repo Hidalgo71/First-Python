768761927ff9ab4687b8673e7cf4d69a02385a9d
Hidalgo71 <y.ozdemir71@gmail.com>
2019-10-02 23:53:20 +0300

    updated.

--- source.py
+++ source.py
@@ -16,3 +16,32 @@ suM = int(int1) + int(int2)
 print(suM)
 print("2nd add:", int(int1) + int(int2))
 
+
+def test_func():
+    print("This is a function")
+    return
+
+
+test_func()
+
+
+def my_func():
+    count = 0
+    num = input("Enter a number: ")
+    num = int(num)
+    for count in num:
+        if count < num:
+            print(count)
+        count += 1
+
+
+my_func()
+
+# iterating list
+my_list = ["a", "b", 71, "c"]
+for i in my_list:
+    print(i)
+# iterating Tuple
+myTuple = ("x", "y", 71, "z")
+for t in myTuple:
+    print(t)
