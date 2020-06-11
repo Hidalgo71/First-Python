1ce6b974500961a07c5e9debac3a550b0102ae25
Hidalgo71 <y.ozdemir71@gmail.com>
2020-06-11 13:57:10 +0300

    Updated.

--- dataSecience.py
+++ dataSecience.py
@@ -4,9 +4,9 @@ import numpy as np
 # No arithmetrical op.
 x = np.array([9, 99, 999])
 print(x[1])
-x[0] = 90                           # Changed value[0]
+x[0] = 90  # Changed value[0]
 print(x)
-print(x.shape)                      # row x column, satır x sütun
+print(x.shape)  # row x column, satır x sütun
 
 print("==========================")
 
@@ -41,4 +41,75 @@ print("==========================")
 
 c = np.random.random((3, 3))
 print(c)
+c = c + 1
+print(c)
+
+print("==========================")
+
+# Array multiplication
+
+c = c * c
+print(c)
+
+c.dot(c)
+print(c)
+print("==========================")
+# matrix multiplication
+d = np.array([1, 2, 3, 4])
+e = np.array([4, 2, 2, 4])
+
+print("d == e ?: ", d == e)
+print("d array multiplication: ", d * d)
+print("d matrix multiplication: ", d.dot(d))
+print("d values > e ?\n", d > e)
+print("==========================")
+# Boll arrays and LOGICAL OR
+k = np.array([1, 1, 0, 0], dtype=bool)
+l = np.array([1, 0, 1, 0], dtype=bool)
+print("k OR l:", np.logical_or(k, l))
+print("k AND l:", np.logical_and(k, l))
+print("==========================")
+# Transcendental(Üstün) functions
+y = np.array([[1, 2, 3], [4, 5, 6]])
+print("Sin: ", np.sin(y))
+print("Log: ", np.log(y))
+print("Exp:%.2f ", np.exp(y))  # %.2f Not working
+
+print("==========================")
+# Rank 2 array shape (3,4) and slicing to pull out the subarray 1st 2 rows and colm 1 and 2 which shape(2,2)
+o = np.array([[1, 2, 3, 4],
+              [5, 6, 7, 8],
+              [9, 10, 11, 12]])
+p = o[:2, 1:2]
+print(p)
+
+print("==========================")
+# Access to the data in the middle row of the array
+rowR1 = o[1, :]
+rowR2 = o[1:2, :]
+print(rowR1, rowR1.shape)
+print(rowR2, rowR2.shape)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
